eebaee94ece99d8760b8bca4d9484a65eea8c8ec
Quinton Fults <quinton.fults.work@gmail.com>
2021-05-20 14:52:48 -0700

    code

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.get('/games/:search', (req, res) => {
   axios.get(`https://api.rawg.io/api/games?key=dd099687bd824636b96236d7352a90da&search=${req.params.search}`)
     .then(r => {
